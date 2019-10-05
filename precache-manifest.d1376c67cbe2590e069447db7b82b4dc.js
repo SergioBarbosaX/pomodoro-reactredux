126a33355fd77ea5da7ced04c94e9585b3439fe4
SergioBarbosaX <serbarbos@hotmail.com>
2019-10-05 13:59:29 +0200

    Updates

---
+++ precache-manifest.d1376c67cbe2590e069447db7b82b4dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f86c5948a53b97a0d83a09cdffeeb89",
+    "url": "/pomodoro-reactredux/index.html"
+  },
+  {
+    "revision": "7827ddba616cf646456b",
+    "url": "/pomodoro-reactredux/static/css/main.b002ec3a.chunk.css"
+  },
+  {
+    "revision": "9a5e3478de769d555abb",
+    "url": "/pomodoro-reactredux/static/js/2.e8228d57.chunk.js"
+  },
+  {
+    "revision": "7827ddba616cf646456b",
+    "url": "/pomodoro-reactredux/static/js/main.d099b28a.chunk.js"
+  },
+  {
+    "revision": "ab434edd560e4c8bbec8",
+    "url": "/pomodoro-reactredux/static/js/runtime~main.092ea6e2.js"
+  }
+]);
